fb19bb0c177326c341b79cd36443538f4c0f2853
Arfandy Adimurfiq <arfandiadimurfiq@gmail.com>
2025-01-31 10:34:17 +0800

    chore: add function to format date from iso format

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,14 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export function formatDate(date: string): string {
+  const newDate = new Date(date);
+  const options: Intl.DateTimeFormatOptions = {
+    day: "2-digit",
+    month: "long",
+    year: "numeric",
+  };
+
+  return new Intl.DateTimeFormat("id-ID", options).format(newDate);
+}
